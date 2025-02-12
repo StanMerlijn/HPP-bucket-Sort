7b8934899bd3c71f401429f2d5f9afe57344dc42
StanMerlijn <144694971+StanMerlijn@users.noreply.github.com>
2025-02-12 20:12:54 +0100

    Plotting new improved bucketsort

--- analysis/plotting_bigO.py
+++ analysis/plotting_bigO.py
@@ -1,22 +1,12 @@
 import matplotlib.pyplot as plt
 import json
 
-# Purely to plot the data for the bucket because matplotlib is fast and easy
-def parse_time(time_str):
-    # Split the value and unit, e.g., "2.4568 ms"
-    value, unit = time_str.split()
-    value = float(value)
-    if unit == "us":
-        return value / 1000
-    elif unit == "ms":
-        return value  
-    elif unit == "s":
-        return value * 1000  #convert seconds to milliseconds
-
 if __name__ == "__main__":
-    type_results = ["results_int", "results_float"]
-    colors = ["blue", "red"]
-    legends = ["Integer sort", "Float sort"]
+    # Initialize the data
+    type_results = ["results_int", "results_float", "results_improved_ints", "results_improved_floats"]
+    colors = ["blue", "red", "green", "orange"]
+    legends = ["Integer sort", "Float sort", "Improved integer sort", "Improved float sort"]
+    samples = [10, 100, 1_000, 10_000, 100_000]
     figure = plt.figure(figsize=(10, 6))
     
     # Load the data
@@ -24,21 +14,12 @@ if __name__ == "__main__":
         data = json.load(f)
     
         for i, test in enumerate(type_results):
-            results = [] 
-            samples = []
-
-            for run in data[test]:
-                results.append(run["est_run_time"])
-                samples.append(run["n"])
-
-            results_parsed = [parse_time(x) for x in results]
-            plt.plot(samples, results_parsed, color=colors[i], label=legends[i])
-
-
-    # Plot the data
+            plt.plot(samples, data[test], color=colors[i], label=legends[i])
+    
     plt.xlabel("n size input")
     plt.ylabel("Time (ms)")
     plt.xticks([x for x in range(0, samples[-1] + 1, 10_000)])
+    plt.yticks([x for x in range(0, 8000, 500)])
     plt.grid()
     plt.legend()
     plt.title("MY Bucket Sort")
